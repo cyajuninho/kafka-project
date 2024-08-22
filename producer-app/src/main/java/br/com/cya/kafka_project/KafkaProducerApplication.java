4b59a38305e827b066446074889a35f726d8087f
cyajuninho <juninho225487@gmail.com>
2024-08-22 19:00:04 -0300

    feat: separating consumer and producer in different app

---
+++ producer-app/src/main/java/br/com/cya/kafka_project/KafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package br.com.cya.kafka_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProducerApplication.class, args);
+	}
+
+}
---
+++ producer-app/src/test/java/br/com/cya/kafka_project/KafkaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cya.kafka_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
