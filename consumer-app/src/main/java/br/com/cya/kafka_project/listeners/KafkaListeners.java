28ce3d495a9fc67cb23e684c185a450f7464229e
cyajuninho <juninho225487@gmail.com>
2024-08-22 20:04:44 -0300

    fix: KafkaListeners name

--- consumer-app/src/main/java/br/com/cya/kafka_project/listeners/KafkaListeners.java
+++ consumer-app/src/main/java/br/com/cya/kafka_project/listeners/KafkaListeners.java
@@ -1,11 +1,12 @@
 package br.com.cya.kafka_project.listeners;
 
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
-public class KafkaListener {
+public class KafkaListeners {
 
-    @org.springframework.kafka.annotation.KafkaListener(topics = "cya-topic", groupId = "cyaGroup")
+    @KafkaListener(topics = "cya-topic", groupId = "cyaGroup")
     public void listener(String message) {
         System.out.println("Received message by consumer: " + message);
     }
